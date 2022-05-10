fe635a0411677d2f7345d7d3fbcfe97544d01a08
Taylor Schley <taylorschley@me.com>
2022-05-10 15:30:18 -0500

    chore: cleanup boilerplate

--- src/router/index.js
+++ src/router/index.js
@@ -9,14 +9,6 @@ const router = createRouter({
       path: '/',
       name: 'dashboard',
       component: DashboardView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/about-view.vue')
     }
   ]
 });
--- src/views/home-view.vue
+++
@@ -1,9 +0,0 @@
-<script setup>
-import TheWelcome from '@/components/the-welcome.vue';
-</script>
-
-<template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
