86e86b8a82fb87889fed25d977e20569cf0b39a5
Taylor Schley <taylorschley@me.com>
2022-07-21 17:41:39 -0500

    fix: fix release script

--- scripts/release.mjs
+++ scripts/release.mjs
@@ -1,5 +1,5 @@
 import process from 'node:process';
-import request from 'superagent';
+import fetch from 'node-fetch';
 import { getOctokit, context } from '@actions/github';
 
 const UPDATER_TAG = 'updater';
@@ -10,9 +10,12 @@ if (process.env.GITHUB_TOKEN === undefined) {
 }
 
 const getSignature = async (url) => {
-  const response = await request.get(url).type('application/octet-stream');
+  const response = await fetch(url, {
+    method: 'GET',
+    headers: { 'Content-Type': 'application/octet-stream' }
+  });
 
-  return response.text;
+  return response.text();
 };
 
 const github = getOctokit(process.env.GITHUB_TOKEN);
@@ -33,7 +36,7 @@ const releaseData = {
       signature: '',
       url: ''
     },
-    'darwin-x86_64': {
+    'darwin-intel': {
       signature: '',
       url: ''
     },
@@ -50,32 +53,48 @@ const releaseData = {
 
 const promises = latestRelease.assets.map(
   async ({ name, browser_download_url }) => {
-    const signature = await getSignature(browser_download_url);
+    // win64 url
+    if (name.endsWith('.msi.zip') && name.includes('en-US')) {
+      releaseData.platforms['windows-x86_64'].url = browser_download_url;
+    }
 
-    if (name.endsWith('.app.tar.gz')) {
+    // win64 signature
+    if (name.endsWith('.msi.zip.sig') && name.includes('en-US')) {
+      const sig = await getSignature(browser_download_url);
+      releaseData.platforms['windows-x86_64'].signature = sig;
+    }
+
+    // darwin url (intel)
+    if (name.endsWith('.app.tar.gz') && !name.includes('aarch')) {
+      releaseData.platforms['darwin-intel'].url = browser_download_url;
+    }
+
+    // darwin signature (intel)
+    if (name.endsWith('.app.tar.gz.sig') && !name.includes('aarch')) {
+      const sig = await getSignature(browser_download_url);
+      releaseData.platforms['darwin-intel'].signature = sig;
+    }
+
+    // darwin url (aarch)
+    if (name.endsWith('aarch64.app.tar.gz')) {
       releaseData.platforms['darwin-aarch64'].url = browser_download_url;
-      releaseData.platforms['darwin-x86_64'].url = browser_download_url;
     }
 
-    if (name.endsWith('.app.tar.gz.sig')) {
-      releaseData.platforms['darwin-aarch64'].signature = signature;
-      releaseData.platforms['darwin-x86_64'].signature = signature;
+    // darwin signature (aarch)
+    if (name.endsWith('aarch64.app.tar.gz.sig')) {
+      const sig = await getSignature(browser_download_url);
+      releaseData.platforms['darwin-aarch64'].signature = sig;
     }
 
+    // linux url
     if (name.endsWith('.AppImage.tar.gz')) {
       releaseData.platforms['linux-x86_64'].url = browser_download_url;
     }
 
+    // linux signature
     if (name.endsWith('.AppImage.tar.gz.sig')) {
-      releaseData.platforms['linux-x86_64'].signature = signature;
-    }
-
-    if (name.endsWith('.msi.zip')) {
-      releaseData.platforms['windows-x86_64'].url = browser_download_url;
-    }
-
-    if (name.endsWith('.msi.zip.sig')) {
-      releaseData.platforms['windows-x86_64'].signature = signature;
+      const sig = await getSignature(browser_download_url);
+      releaseData.platforms['linux-x86_64'].signature = sig;
     }
   }
 );
@@ -83,15 +102,19 @@ const promises = latestRelease.assets.map(
 await Promise.allSettled(promises);
 
 if (!releaseData.platforms['darwin-aarch64'].url) {
-  throw new Error('Failed to get release for MacOS');
+  console.error('Failed to get release for MacOS (aarch64)');
+}
+
+if (!releaseData.platforms['darwin-intel'].url) {
+  console.error('Failed to get release for MacOS (intel)');
 }
 
 if (!releaseData.platforms['linux-x86_64'].url) {
-  throw new Error('Failed to get release for Linux');
+  console.error('Failed to get release for Linux');
 }
 
 if (!releaseData.platforms['windows-x86_64'].url) {
-  throw new Error('Failed to get release for Windows');
+  console.error('Failed to get release for Windows');
 }
 
 const { data: updater } = await repos.getReleaseByTag({
@@ -113,5 +136,5 @@ await repos.uploadReleaseAsset({
   ...repoMetaData,
   release_id: updater.id,
   name: UPDATER_FILE,
-  data: JSON.stringify(releaseData)
+  data: JSON.stringify(releaseData, null, 2)
 });
