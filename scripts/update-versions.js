f9eca7b702079a6eb8fb6402775ea906bb6876a8
Taylor Schley <taylorschley@me.com>
2022-07-21 12:29:08 -0500

    fix: fix update-version file

--- scripts/update-versions.js
+++ scripts/update-versions.js
@@ -2,11 +2,14 @@ const fs = require('fs/promises');
 const path = require('path');
 
 (async () => {
-  const packageJson = await fs.readFile('./package.json', 'utf8');
+  const packageJson = await fs.readFile(
+    path.join(__dirname, '../package.json'),
+    'utf8'
+  );
   const packageJsonObject = JSON.parse(packageJson);
   const version = packageJsonObject.version;
 
-  console.log(`Updating 'tauri.conf.json' file...`);
+  console.log(`Updating 'tauri.conf.json' file with version ${version}...`);
   const tauriConfPath = path.join(__dirname, '../src-tauri/tauri.conf.json');
   const tauriConfJson = await fs.readFile(tauriConfPath, 'utf8');
   const tauriConfJsonObject = JSON.parse(tauriConfJson);
